0d9de8d15c3dd2aa5cf2c86b7f702071afe379e6
akora <AKORA>
2022-10-23 14:28:45 +0200

    add bianry

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,51 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::str;
-//use std::io::SeekFrom;
+
+const FILE_PATH: &str = "./pi.txt";
+const SEQUENCE: &str = "694208008";
 
 fn main() -> io::Result<()> {
-    let file_path = "./pi.txt";
-    let mut f = File::open(file_path)?;
-    println!("PI file {}", file_path);
-
-    let seq = find_sequence("314", &mut f);
-    println!("Seq: {:?}", seq);
+    let mut f = File::open(FILE_PATH)?;
+    println!("PI file:  \"{}\"", FILE_PATH);
+    println!("Sequence: \"{}\"", SEQUENCE);
+    let seq_pos = find_sequence(SEQUENCE, &mut f).expect("Sequence found");
+    println!("Sequence found at position {seq_pos}");
 
     Ok(())
 }
 
 const BLOCK_SIZE: usize = 1024 / 2;
 
-fn find_sequence(str_seq: &str, f: &mut File) -> io::Result<()> {
+fn find_sequence(str_seq: &str, f: &mut File) -> io::Result<usize> {
     let mut buffer = [0; BLOCK_SIZE];
     let seq: &[u8] = str_seq.as_bytes();
-  
+    let seq_len = seq.into_iter().count();
+
     //Every digit is 2 bytes
-    let pos: u64 = 0;
-    f.seek(io::SeekFrom::Start(pos))?;
-    // up to one block
-    let n = f.read(&mut buffer[..])?;
-    //buffer.
-    println!("Buffer len: {n}");
+    let mut pos: usize = 0;
 
-    let s = str::from_utf8(&buffer).expect("L");
-    
-    println!("Buffer: {s}");
-    //Search squenze in specific Block
+    loop {
+        f.seek(io::SeekFrom::Start(pos as u64))?;
+        // up to one block
+        let _n = f.read(&mut buffer[..])?;
+        //buffer.
+        //println!("Buffer len: {n}");
 
-    //Seek to next block -> redo until found
-    Ok(())
+        for i in 0..(BLOCK_SIZE - seq_len) {
+            let buf_slice = &buffer[i..(i + seq_len)];
+            //let sli = str::from_utf8().expect("L");
+            //Search squenze in specific Block
+            let same = seq == buf_slice;
+            if same {
+                return Ok(pos+i);
+            }
+        }
+
+        //let s = str::from_utf8(&buffer).expect("L");
+        //println!("Buffer: {s}");
+
+        //Seek to next block -> redo until found
+        pos += BLOCK_SIZE;
+    }
 }
---
+++ src/test.rs
@@ -0,0 +1,62 @@
+///Convert the pi.txt file to a binary file
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+use std::str;
+
+const TEXT_PATH: &str = "./pi.txt";
+const BIN_PATH: &str = "./pi.bin";
+const BLOCK_SIZE: usize = 1024 * 8;
+
+fn main() -> io::Result<()> {
+    let mut txt_f = File::open(TEXT_PATH)?;
+    println!("opened textfile at {TEXT_PATH}");
+    
+    let mut bin_f = File::create(BIN_PATH)?;
+    println!("created binfile at {BIN_PATH}");
+    
+    
+    let mut cur_buffer = [0; BLOCK_SIZE];
+    let mut cur_pos: usize = 0;
+    let file_length = txt_f.metadata().expect("Couldn't read length").len();
+
+    while (cur_pos as u64) < file_length {
+        //Seek forwards another block
+        //f.seek(io::SeekFrom::Current(BLOCK_SIZE as i64))?;
+        
+        //Read one block into current buffer
+        let _n = txt_f.read(&mut cur_buffer[..])?;
+
+        //Convert bytes to string
+        let sli = str::from_utf8(&cur_buffer).expect("Couldn't convert bytes to string");
+        
+        //Convert string slice to numbers
+        let nums: Vec<u8> = sli.chars().map(char_to_num).collect();
+        bin_f.write(&nums)?;
+
+        println!("{} / {}", cur_pos/BLOCK_SIZE, file_length/(BLOCK_SIZE as u64));
+
+        cur_pos += BLOCK_SIZE;
+    }
+
+    println!("Done");
+
+    Ok(())
+}
+
+
+fn char_to_num(c: char) -> u8 {
+    match c {
+        '0' => 0,
+        '1' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        _ => 0
+    }
+}
