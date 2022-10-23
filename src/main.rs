2953f5c3692b2258e8a2c766465cb5d94944cf83
akora <AKORA>
2022-10-23 15:15:36 +0200

    a

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::io::prelude::*;
 use std::str;
 
 const FILE_PATH: &str = "./pi.txt";
-const SEQUENCE: &str = "694208008";
+const SEQUENCE: &str = "6942000";
 
 fn main() -> io::Result<()> {
     let mut f = File::open(FILE_PATH)?;
-    println!("PI file:  \"{}\"", FILE_PATH);
-    println!("Sequence: \"{}\"", SEQUENCE);
-    let seq_pos = find_sequence(SEQUENCE, &mut f).expect("Sequence found");
-    println!("Sequence found at position {seq_pos}");
+    println!("text file {FILE_PATH}");
+    println!("sequence {SEQUENCE}");
+    let seq_pos = find_sequence(SEQUENCE, &mut f)?;
+    println!("sequence found at position {seq_pos}");
 
     Ok(())
 }
@@ -19,34 +19,29 @@ fn main() -> io::Result<()> {
 const BLOCK_SIZE: usize = 1024 / 2;
 
 fn find_sequence(str_seq: &str, f: &mut File) -> io::Result<usize> {
-    let mut buffer = [0; BLOCK_SIZE];
     let seq: &[u8] = str_seq.as_bytes();
     let seq_len = seq.into_iter().count();
 
-    //Every digit is 2 bytes
-    let mut pos: usize = 0;
+    let mut cur_buffer = [0; BLOCK_SIZE];
+    let mut cur_pos: usize = 0;
+    let file_length = f.metadata()?.len();
 
-    loop {
-        f.seek(io::SeekFrom::Start(pos as u64))?;
-        // up to one block
-        let _n = f.read(&mut buffer[..])?;
-        //buffer.
-        //println!("Buffer len: {n}");
+    while (cur_pos as u64) <= file_length {
+        //Read one block into current buffer
+        let _n = f.read(&mut cur_buffer[..])?;
 
         for i in 0..(BLOCK_SIZE - seq_len) {
-            let buf_slice = &buffer[i..(i + seq_len)];
-            //let sli = str::from_utf8().expect("L");
-            //Search squenze in specific Block
+            let buf_slice = &cur_buffer[i..(i + seq_len)];
+            //Search sequence in specific Block
             let same = seq == buf_slice;
             if same {
-                return Ok(pos+i);
+                return Ok(cur_pos+i - 2);
             }
         }
 
-        //let s = str::from_utf8(&buffer).expect("L");
-        //println!("Buffer: {s}");
 
-        //Seek to next block -> redo until found
-        pos += BLOCK_SIZE;
+        cur_pos += BLOCK_SIZE;
     }
+
+    Err(io::Error::new(io::ErrorKind::Other, "Didn't find sequence"))
 }
